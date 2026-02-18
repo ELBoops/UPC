b579326ab8df7e12e2c8340d56255174d962fe5e
ELBoops <kurikurganova@gmail.com>
2026-02-18 10:59:00 +0500

    Хреновый коммит, но что поделать. Переписываем всю хрень. Слова в ветер

--- gui/screens/main_screen.py
+++ gui/screens/main_screen.py
@@ -8,6 +8,11 @@ class MainScreen(Screen):
         super().__init__(**kwargs)
         
         bl = BoxLayout(orientation="vertical", spacing=20, padding=40)
+        
+        def chage_screen(screen_name):
+            def change_screen_inner(instance):
+                self.run_gui(screen_name)
+            return change_screen_inner
 
         bl.add_widget(Label(
             text="Выберите предмет",
@@ -17,18 +22,22 @@ class MainScreen(Screen):
         
         bl.add_widget(Button(
             text="Maths",
+            on_press= chage_screen("maths_screen")
         ))
 
         bl.add_widget(Button(
             text="Physics",
+            on_press= chage_screen("physics_screen")
         ))
 
         bl.add_widget(Button(
             text="Chemistry",
+            on_press= chage_screen("chemistry_screen")
         ))
 
         bl.add_widget(Button(
             text="Custom",
+            on_press= chage_screen("custom_screen")
         ))
 
         self.add_widget(bl)
